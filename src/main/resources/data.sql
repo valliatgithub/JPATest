5626cfbc6938b85df3c418b05cbcb6b150ba5952
valliatgithub <mythilijag@gmail.com>
2020-02-20 16:33:45 -0500

    test commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into alien values(214,'Mythili','Java');
+insert into alien values(215,'test','oracle');
+insert into alien values(216,'abc','phython');
+insert into alien values(217,'techy','nodejs');
