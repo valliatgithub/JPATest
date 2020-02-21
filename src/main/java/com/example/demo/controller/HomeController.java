eea4d0eeba8c1f14eb5154ba469516089671d591
valliatgithub <mythilijag@gmail.com>
2020-02-20 19:02:39 -0500

    test commit

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,30 +1,22 @@
 package com.example.demo.controller;
 
-import java.util.Optional;
 
-import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.dao.AlienDao;
-import com.example.demo.model.Alien;
 @Controller
 public class HomeController {
-@Autowired
-AlienDao alienDao;
 @RequestMapping("/")
 public String home() {
 	System.out.println("home calling");
 	return "home.jsp";
 }
-@RequestMapping("/addAlien/{id}")
+@RequestMapping("/addAlien/{name}")
 @ResponseBody
-public Optional<Alien> addAlien(@PathVariable ("id")int id) {
-	return alienDao.findById(id);
+public String addAlien(@PathVariable ("name")String name) {
+	return "hello"+name;
 }
 }
--- src/main/java/com/example/demo/dao/AlienDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.model.Alien;
-
-public interface AlienDao extends CrudRepository<Alien,Integer> {
-
-}
