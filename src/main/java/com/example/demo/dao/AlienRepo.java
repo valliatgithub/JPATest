248b400675d9b0152cd1fafe1fbd3c9d31281ff0
valliatgithub <mythilijag@gmail.com>
2020-02-20 20:12:43 -0500

    test commit

---
+++ src/main/java/com/example/demo/dao/AlienRepo.java
@@ -0,0 +1,14 @@
+package com.example.demo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.Alien;
+
+import java.util.List;
+
+public interface AlienRepo extends JpaRepository<Alien,Integer>
+{
+
+}
