f82cdecac7521fb1110c6fe0db76f94321e34194
valliatgithub <mythilijag@gmail.com>
2020-03-13 23:53:57 -0400

    using thymeleaf

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into alien values (101,'Navin','Java');
-insert into alien values (102,'Kiran','Android');
-insert into alien values (103,'Pravin','Python');
-insert into alien values (104,'Komal','Java');
-insert into alien values (105,'Rafiq','Android');
